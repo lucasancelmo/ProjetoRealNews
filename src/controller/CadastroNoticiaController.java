8b8641172199498008d382a9b3556874e52930ba
joaovbds <55898458+joaovbds@users.noreply.github.com>
2020-04-26 17:51:28 -0300

    Ajustes na servlete com base na service de noticia

--- src/controller/CadastroNoticiaController.java
+++ src/controller/CadastroNoticiaController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import model.Noticia;
+import service.NoticiaService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,13 +31,27 @@ public class CadastroNoticiaController extends HttpServlet {
 		String til = request.getParameter("titulo");
 		String desc = request.getParameter("descricao");
 		String tex = request.getParameter("texto");
+		
+		// LUCAS ESTOU ALTERANDO ESSE SERVLET PQ CRIEI A SERVICE ACREDITO QUE SEJA ASSIM QUE FAÇA
+		
+		// Instaciamento da Noticia 
+		Noticia noticia = new Noticia();
+		noticia.setTitulo(til);
+		noticia.setDescricao(desc);
+		noticia.setTexto(tex);
+		
+		// instaciamento da Service
+		NoticiaService cs = new NoticiaService();
+		cs.criar(noticia);
+		noticia = cs.selecionar(noticia.getId());
+		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		request.getRequestDispatcher("index.html").include(request,  response);	
 		out.println("Noticia cadastrada<br>");
-		out.println("Titulo " + til+ "<br>");
-		out.println("Descricao " + desc + "<br>");
-		out.println("Texto: " + tex+ "<br>");
+		out.println("Titulo " + noticia.getTitulo() + "<br>");
+		out.println("Descricao " + noticia.getDescricao() + "<br>");
+		out.println("Texto: " + noticia.getTexto() + "<br>");
 			
 	}
 
