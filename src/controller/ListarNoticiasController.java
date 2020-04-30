a6431e1d4e6a494b793fe02916e083deb527c55c
joaovbds <55898458+joaovbds@users.noreply.github.com>
2020-04-30 19:35:06 -0300

    Controller Listar Noticia

--- src/controller/ListarNoticiasController.java
+++ src/controller/ListarNoticiasController.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import service.NoticiaService;
+import model.Noticia;
 
 /**
  * Servlet implementation class ListarNoticiasController
@@ -26,6 +28,22 @@ public class ListarNoticiasController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		NoticiaService ns = new NoticiaService();
+		List<Noticia> ln = new ArrayList<Noticia>();
+		ln = ns.selecionarNoticias();
+		
+		output.println("<script> "
+				+ "$(document).ready(function(){");
+		for(Noticia n : ln) {
+			String til = "\"<p class=\'text-center text-justify\'>"+ n.getTitulo() + "</p>\"";
+			String desc = "\"<p class=\'text-center text-justify\'>"+ n.getDescricao() + "</p>\"";
+			output.println("$('#til').append(" + til +");");
+			output.println("$('#desc').append(" + desc +");");
+		}
+		output.println("});");
+		output.println("</script>");
+		
+		request.getRequestDispatcher("index.html").include(request,  response);
 		
 	}
 
